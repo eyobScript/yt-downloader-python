c7abe892e96ddd4336901604d04d57040e57f3e1
eyobScript <eyobbmulugeta@gmail.com>
2025-03-10 16:54:50 +0300

    create a function that ask user for save video

--- youtube.py
+++ youtube.py
@@ -20,6 +20,11 @@ def download_video(url, save_path="."):
         print(f"Error: {e}")
 
 
+def open_file_dialog():
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        print(f"Selected folder: {folder_path}")
+    return folder_path
 # video_url = "https://youtu.be/g_Pog2fAuAE?si=b09NM7gHzTwQcgVz"
 # download_video(video_url, save_path="./downloads")
 
