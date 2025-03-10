b25615664b07251b26f0f590631819a0e3e486ad
eyobScript <eyobbmulugeta@gmail.com>
2025-03-10 16:14:54 +0300

    create the video download logics

--- youtube.py
+++ youtube.py
@@ -3,4 +3,11 @@ import tkinter as tk
 from tkinter import filedialog
 
 def download_video(url, save_path):
-    pass
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.filter(file_extension="mp4", progressive=True)
+        highest_res_stream = stream.get_highest_resolution()
+        highest_res_stream.download(output_path=save_path)
+        print("Video Downloaded Successfully!")
+    except Exception as e:
+        print(e)
